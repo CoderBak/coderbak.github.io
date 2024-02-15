e8b661e292ad556bd1e5c208ce76a577e6090341
CoderBak <hx-sun@outlook.com>
2024-02-15 08:06:06 +0000

    Update

--- _static/js/math-csr.js
+++ _static/js/math-csr.js
@@ -1,18 +1,4 @@
-MathJax = {
-  chtml: {
-    scale: 1.0,
-    matchFontHeight: false
-  },
-  options: {
-    enableMenu: true,
-    menuOptions: {
-      settings: {
-        renderer: 'CHTML',
-        scale: 1.0
-      }
-    }
-  }
-};
+
 
 document$.subscribe(() => {
     MathJax.startup.output.clearCache()
