ed93db89b47aa16208fdfb6f57106e9937825288
CoderBak <hx-sun@outlook.com>
2024-02-15 08:07:33 +0000

    Update

--- _static/js/math-csr.js
+++ _static/js/math-csr.js
@@ -1,8 +1,22 @@
-
+/*MathJax = {
+  chtml: {
+    scale: 1.0,
+    matchFontHeight: false
+  },
+  options: {
+    enableMenu: true,
+    menuOptions: {
+      settings: {
+        renderer: 'CHTML',
+        scale: 1.0
+      }
+    }
+  }
+};*/
 
 document$.subscribe(() => {
-    MathJax.startup.output.clearCache()
-    MathJax.typesetClear()
-    MathJax.texReset()
+    //MathJax.startup.output.clearCache()
+    //MathJax.typesetClear()
+    //MathJax.texReset()
     MathJax.typesetPromise()
   })
