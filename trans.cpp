8ab76f35a2ba4bf8726cad6a9717fa16e4f246d1
CoderBak <hx-sun@outlook.com>
2024-06-09 04:43:45 +0000

    Update

--- trans.cpp
+++ trans.cpp
@@ -12,7 +12,7 @@ int main() {
     std::regex pattern1(R"(<div class="md-sidebar md-sidebar--secondary")");
     std::string replacement1 = R"($& id="index-toc")";
     std::string replacedContent = std::regex_replace(content, pattern1, replacement1);
-    std::regex pattern2(R"(<div class="md-sidebar__scrollwrap"> style=")");
+    std::regex pattern2(R"(<div class="md-sidebar__scrollwrap" style=")");
     std::string replacement2 = R"(<div itemscope="" itemtype="http://schema.org/Person" class="profile_box" style="padding-left: .5rem;padding-right: 1rem;"><div class="author__avatar"><img src="https://coderbak.com/document/avatar.png" class="author__avatar"></div>
   <div class="author__content">
     <hr><h3 class="author__name" style="font-size: .9rem !important;">Haoxiang Sun</h3>
